ce1c0b3ceb20e1769667c3e41fb1c57c6e4266a0
Joshua Blake <jbb50@cam.ac.uk>
2020-03-17 16:08:59 +0000

    clang-format

--- src/gp_param_patch.h
+++ src/gp_param_patch.h
@@ -4,6 +4,8 @@
 #include "RTM_Header.h"
 #include "RTM_StructDefs.h"
 
-void regional_matrix_parameter_GP_PATCH(gsl_matrix*, const gsl_vector*, const regression_def&, const int, const int);
+void regional_matrix_parameter_GP_PATCH(gsl_matrix *, const gsl_vector *,
+                                        const regression_def &, const int,
+                                        const int);
 
 #endif
