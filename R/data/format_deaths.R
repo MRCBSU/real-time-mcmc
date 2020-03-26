7aa5b4b0ac962b514c336f466e5240169a4b663b
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-03-26 14:28:44 +0000

    Restructure format_deaths.R to bring into line with format_linelist.R

--- R/data/format_deaths.R
+++ R/data/format_deaths.R
@@ -1,12 +1,20 @@
+library(assertr)
 library(readr)
 library(lubridate)
 library(dplyr)
 
-## Get deaths data
+#########################################################
+## Inputs that should (or may) change on a daily basis
+#########################################################
 
-## Inputs that should change on a daily basis.
-date.data <- "20200324"
-dir.data <- "../../../Data/"
+## Map our names for columns (LHS) to data column names (RHS)
+col.names <- list(
+	death_date = "dod",
+	finalid = "finalid"
+)
+
+## YYYYMMDD string, used in filenames and reporting lag
+date.data <- "20200325"
 
 ## Inputs that are dependent on the output required.
 reporting.delay <- 2
@@ -14,9 +22,6 @@ reporting.delay <- 2
 region.def.str <- "\"ENGLAND\""
 ## death.col.name <- "PATIENT_DEATH_DATE"
 
-## Column name that contains the date of event
-if(!exists("death.col.name"))
-    death.col.name <- "Date of death"
 
 ####################################################################
 ## BELOW THIS LINE SHOULD NOT NEED EDITING
@@ -51,23 +56,33 @@ death.col.args[[col.names[["death_date"]]]] <- col_character()
 death.col.args[[col.names[["finalid"]]]] <- col_double()
 death.cols <- do.call(cols, death.col.args)	# Calling with a list so use do.call
 
+## Read the file and rename columns
+dth.dat <- read_csv(
+		build.data.filepath(subdir = "raw", date.data, " COVID19 Deaths.csv"),
+		col_types = death.cols
+	) %>%
+	rename(!!!col.names) %>%
+    mutate(Date = fuzzy_date_parse(death_date)) %>%
+	## Remove two lines with implausible death dates
+	filter(finalid != 9425 & finalid != 30609) %>%
+	## Check plausibility (should flag parsing errors)
+	verify(Date >= ymd("2020-01-01")) %>%
+	verify(Date <= today())
 
 ## The following code was necessary for the first time on 20200324. Hopefully it can be commented out and ignored in future iterations
-dth.dat <- read_csv(paste0("../../../Data/Deaths/", date.data, " COVID19 Deaths.csv"))
-dth.dat$dod <- lubridate::as_date(dth.dat$dod, format = "%d/%m/%Y", tz = "GMT")
-dth.dat$dod_NHSE <- lubridate::as_date(dth.dat$dod_NHSE, format = "%m/%d/%Y", tz = "GMT")
-x <- dth.dat$dod
-x[is.na(x)] <- dth.dat$dod_NHSE[is.na(x)]
-dth.dat <- dth.dat %>%
-    mutate(Date = x)
+#dth.dat$dod <- lubridate::as_date(dth.dat$dod, format = "%d/%m/%Y", tz = "GMT")
+#dth.dat$dod_NHSE <- lubridate::as_date(dth.dat$dod_NHSE, format = "%m/%d/%Y", tz = "GMT")
+#x <- dth.dat$dod
+#x[is.na(x)] <- dth.dat$dod_NHSE[is.na(x)]
+#dth.dat <- dth.dat %>%
+    #mutate(Date = x)
 ## ## 
 
-latest.date <- lubridate::as_date(date.data) - reporting.delay
-earliest.date <- lubridate::as_date("2020-02-17")
+latest.date <- ymd(date.data) - reporting.delay
+earliest.date <- ymd("2020-02-17")
 all.dates <- as.character(seq(earliest.date, latest.date, by = 1))
 
 dth.dat <- dth.dat %>%
-    ## mutate(Date = lubridate::as_date(apply(dth.dat, 1, function(x) as.Date(as.character(x[death.col.name]), format = "%d/%m/%Y")))) %>%
     filter(Date <= latest.date) %>%
     filter(Date >= earliest.date) %>%
     mutate(fDate = factor(Date)) %>%
@@ -86,7 +101,7 @@ rtm.dat <- arrange(rtm.dat, fDate)
 for(reg in levels(rtm.dat$Region)){
     write.table(filter(rtm.dat, Region == reg) %>%
                select(fDate, count),
-            file = paste0("../../data/deaths/deaths", date.data, "_", reg, ".txt"),
+            file = build.data.filepath("RTM_format", "deaths", date.data, "_", reg, ".txt"),
             sep = "\t",
             col.names = FALSE,
             row.names = FALSE)
