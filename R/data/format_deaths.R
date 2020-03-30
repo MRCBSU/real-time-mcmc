476b0858ab095bb963f560cd991dd2c079626ffb
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-03-30 17:18:54 +0100

    Move from manually specifying rows with errors to doing so heuristically
    We use the following algorithm:
    
    1. Consider a date (onset or death) plausible if it's between 1st Jan
    2020 and whatever today's date is.
    2. If a date is implausible, try swapping the day and month and see if
    that is now plausible.
    3. If that is now plausible (and it wasn't previously), accept the
    swap.
    4. Output as implausible anything that the fix didn't help with and
    rows with onset date after death date (if both exist).
    5. Output the rows we have swapped (in case the heuristic goes wonky).
    
    This gives plausible results for everything in the latest (29th Mar)
    deaths data except finalids 9424 and 30609 (with death dates 2018-08-19
    and 2019-09-18 respectively).
    Note that, with the latest data, we only swap onset (not death) dates.

--- R/data/format_deaths.R
+++ R/data/format_deaths.R
@@ -64,24 +64,35 @@ death.col.args[[col.names[["onset_date"]]]] <- col_character()
 death.col.args[[col.names[["finalid"]]]] <- col_double()
 death.cols <- do.call(cols, death.col.args)	# Calling with a list so use do.call
 
+within.range <- function(dates) {
+	return(dates <= today() & dates >= ymd("2020-01-01"))
+}
+
 plausible.death.date <- function(x) {
-	within.range <- x$Date <= today() & x$Date >= ymd("2020-01-01")
+	death.within.range <- within.range(x$Date)
+	onset.within.range <- is.na(x$Onset) | within.range(x$Onset)
 	after.onset <- is.na(x$Onset) | (x$Onset <= x$Date)
-	return(within.range & after.onset)
+	return(death.within.range & onset.within.range & after.onset)
+}
+
+heuristically.swap.day.and.month <- function(dates) {
+	swapped.dates <- suppressWarnings(swap.day.and.month(dates))
+	should.swap <- (!is.na(swapped.dates) &
+					!within.range(dates) &
+					within.range(swapped.dates))
+	dates[should.swap] <- swapped.dates[should.swap]
+	return(dates)
 }
 
 ## Some patients are known to have the month and day swapped
 ## Fix these here...
 fix.dates <- function(df) {
-	finalid.swapped.onset <- c(2507, 2727, 3159, 3284, 3767, 6969, 35367, 39214)
-	finalid.swapped.death <- c(2507)
-	df <- df %>% mutate(
-		swap_onset = finalid %in% finalid.swapped.onset,
-		swap_death = finalid %in% finalid.swapped.death
-	)
-	df[df$swap_death,]$Date  = swap.day.and.month(df[df$swap_death,]$Date)
-	df[df$swap_onset,]$Onset = swap.day.and.month(df[df$swap_onset,]$Onset)
-	return(df)
+	return(mutate(df,
+		orig_date = Date,
+		orig_onset = Onset,
+		Date = heuristically.swap.day.and.month(Date),
+		Onset = heuristically.swap.day.and.month(Onset)
+	  ))
 }
 
 ## Read the file and rename columns
@@ -104,9 +115,21 @@ dth.dat <- read_csv(
 if (!all(dth.dat$plausible_death_date)) {
 	implausible.dates <- dth.dat %>% filter(!plausible_death_date)
 	print("WARNING: The following rows have implausible death dates and have been excluded: ")
-	print(implausible.dates %>% select(c(finalid, Date, Onset, swap_death, swap_onset)))
+	implausible.dates %>% select(c(finalid, Date, Onset)) %>% print(n=1000)
 	dth.dat <- dth.dat %>% filter(plausible_death_date)
 }
+
+print("WARNING: the following rows have had onset and/or death date changed to become plausible: ")
+dth.dat %>%
+   filter(orig_date != Date | orig_onset != Onset) %>%
+   select(c(finalid, Date, orig_date, Onset, orig_onset)) %>%
+   mutate(
+   		orig_date = as_date(ifelse(orig_date == Date,
+							  NA, orig_date)),
+   		orig_onset = as_date(ifelse(orig_onset == Onset,
+							  NA, orig_onset))
+	) %>%
+   print(n=1000)
 
 ## The following code was necessary for the first time on 20200324. Hopefully it can be commented out and ignored in future iterations
 #dth.dat$dod <- lubridate::as_date(dth.dat$dod, format = "%d/%m/%Y", tz = "GMT")
