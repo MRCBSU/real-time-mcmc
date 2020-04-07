82efc1cd84346bb53a9a4d5dfe80b342c2276e2d
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-04-07 16:25:31 +0100

    Configure to run all regions

--- config.R
+++ config.R
@@ -2,17 +2,29 @@
 ## THIS FILE CONTAINS GENERAL PARAMETERS NEEDING TO BE UPDATED
 #######################################################################
 
-date.data <- "20200403"
+date.data <- "20200406"
 
 
 # Number of days to run the simulation for.
 # Including lead-in time, analysis of data and short-term projection
-ndays <- 62
+ndays <- 73
 
+all.regions <- c(
+	"East_of_England",
+	"London",
+	"Midlands",
+	"North_East_and_Yorkshire",
+	"North_West",
+	"South_East",
+	"South_West"
+)			# Regions under study
+args <- commandArgs(trailingOnly = TRUE)
+region.index <- as.integer(args[length(args)])
+if (length(region.index) == 0) region.index <- 1
+regions <- all.regions[region.index]
 
-regions <- "ENGLAND"
 # Choose the name of the subdirectory in model_runs to use
-subdir.name <- paste0(date.data, "regions_alone")
+subdir.name <- paste0(date.data, "_regions_alone_new")
 out.dir <- file.path(proj.dir, "model_runs", subdir.name, regions)	# Value actually used
 
 scenario.name <- "variable"
