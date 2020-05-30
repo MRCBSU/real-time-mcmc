8e7bdb192c99eb29bac1a406d7de90aa0348fd04
Joshua Blake <joshuab@mrc-bsu.cam.ac.uk>
2020-05-30 14:02:28 +0100

    Comment calculation of Rt

--- R/output/tidy_output.R
+++ R/output/tidy_output.R
@@ -125,28 +125,37 @@ if(rw.flag)
     m <- m %*% t(m.design)
 ## Inverse transformation
 m <- exp(m)
+# m now has the parameters on natural scale
 if(ncol(m) %% r != 0) {
   warning('Number of m parameters is not a multiple of number of regions, cannot caclulate Rt')
 } else {
   m.per.region <- ncol(m) / r
   R0 <- posterior.R0[iterations.for.Rt, , drop = F];colnames(R0) <- regions
   for(idir in 1:length(cm.bases)){
+	# Read the matrices containing the transmission between age groups (from Ediwn)
     M[[idir]] <- as.matrix(read_tsv(cm.bases[idir], col_names = FALSE, col_types = cols()))
+    # Read matrices which select which elements of m to use, add one because R uses 1-based indexing
+    # but the model uses 0-based
     M.mult[[idir]] <- as.matrix(read_tsv(cm.mults[idir], col_names = FALSE, col_types = cols())) + 1
   }
+  # m.levels[t] is the number of breakpoints passed on day t
   m.levels <- cut(1:ndays, c(0, cm.breaks, Inf))
   names(M) <- names(M.mult) <- NULL
   pop.total <- all.pop[1, ];names(pop.total) <- regions
   for(reg in regions){
     ireg <- which(regions %in% reg)
     for(idir in 1:length(cm.bases)){
+	  # M.star[i] <- m[i] * M[i] but select the correct m for the region
       M.star[[idir]] <- array(apply(m, 1, function(mm) M[[idir]] * mm[(ireg-1)*m.per.region+M.mult[[idir]]]),
                               dim = c(nA, nA, nrow(m)))
     }
     M.temp <- matrix(M.star[[1]][, , 1, drop = F], dim(M.star[[1]])[1], dim(M.star[[1]])[2])
+	# Calculate the scaling R*, the value of Rt for the first matrix (unscaled) TODO: Check this description is correct.
     R.star[ireg] <- Rt.func(regions.total.population[ireg, ] / pop.total[ireg], M.temp)
+	# Calculate number of susceptiables for each day
     S <- apply(NNI[[reg]], c(1, 3), cumsum)[,,seq(from = 1, to = length(parameter.to.outputs), length.out = 500)]  ## TxAxI array
     S <- -sweep(S, 2, regions.total.population[ireg, ], `-`) ## TxAxI
+	# Calculate the relative Rt values as a function of the next generation matrix for each day
     R.prime <- sapply(1:ndays,
                       function(x) sapply(1:length(iterations.for.Rt), function(i){
                         M.temp <- matrix(M.star[[m.levels[x]]][, , i, drop = F], nA, nA)
@@ -154,6 +163,7 @@ if(ncol(m) %% r != 0) {
                       }
                       )
     ) ## IxT array
+	# Scale the relative Rt values to give the correct R0s
     Rt[[reg]] <- R0[, ireg] * R.prime / R.star[ireg] ## I*T array
   }
   names(R.star) <- regions
