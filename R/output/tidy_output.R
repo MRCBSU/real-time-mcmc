31b13717b5ea16e117f21cf0225b18c3b610896c
pjbirrell <paul.birrell@phe.gov.uk>
2020-06-05 04:40:11 +0100

    Pushing changes to avoid over-writing in merge.

--- R/output/tidy_output.R
+++ R/output/tidy_output.R
@@ -106,8 +106,8 @@ parameter.iterations <- int_iter[(!((int_iter + 1 - burnin) %% thin.params)) & i
 outputs.iterations <- int_iter[(!((int_iter + 1 - burnin) %% thin.outputs)) & int_iter >= burnin]
 parameter.to.outputs <- which(parameter.iterations %in% outputs.iterations)
 stopifnot(length(parameter.to.outputs) == length(outputs.iterations)) # Needs to be subset
-save.image("tmptmp.RData")
-stop()
+## save.image("tmptmp.RData")
+## stop()
 ################################################################
 print('Calculating Rt')
 Rt.func <- function(vecS, matM){
