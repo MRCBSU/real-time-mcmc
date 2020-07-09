3c6205701dafd9dda04b3d07cb7544c01901e309
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-07-09 15:39:32 +0100

    Always redo HTML when using rerun.R

--- rerun.R
+++ rerun.R
@@ -3,21 +3,19 @@ load("tmp.RData")
 
 out.dir <- getwd()
 
-run.outputs <- !run.outputs
-
 require(rmarkdown)
 
 Rfile.loc <- file.path(file.loc, "R/output")
 
-if(run.outputs){
-    source(file.path(Rfile.loc, "tracePlots.R"))
-	rmarkdown::render(
-		file.path(Rfile.loc, 'report-updated.Rmd'),
-		html_document(pandoc_args = "--self-contained"),
-		output_dir = out.dir,
-		clean = FALSE, intermediates_dir = out.dir
-	)
+if (!file.exists("mcmc.RData")) {
+	source(file.path(Rfile.loc, "tracePlots.R"))
 }
+rmarkdown::render(
+	file.path(Rfile.loc, 'report-updated.Rmd'),
+	html_document(pandoc_args = "--self-contained"),
+	output_dir = out.dir,
+	clean = FALSE, intermediates_dir = out.dir
+)
 
 ## Return back to initial directory
 setwd(startwd)
