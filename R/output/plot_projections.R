e2a06256fbdc95c0e088ba38e172a2398c3fa6ef
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-10-24 14:31:00 +0100

    Refactor to allow a midterm report to be produced

---
+++ R/output/plot_projections.R
@@ -0,0 +1,18 @@
+load("tmp.RData")
+out.dir <- getwd()
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 1) {
+	projections_file <- args[1]
+}
+
+require(rmarkdown)
+
+Rfile.loc <- file.path(file.loc, "R/output")
+external <- FALSE
+
+rmarkdown::render(
+	file.path(Rfile.loc, 'projinf-report.Rmd'),
+	html_document(pandoc_args = "--self-contained"),
+	output_dir = out.dir,
+	output_file = paste0(projections_file, ".html")
+)
