9e0ce0626a43120a5c77dae05f5ea592bc9103b8
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2020-10-28 15:44:48 +0000

    Changes to be suitable to merge

--- config.R
+++ config.R
@@ -5,10 +5,10 @@ library(lubridate)
 library(tidyr)
 
 # Either ONS or NHS
-region.type <- "ONS"
+region.type <- "NHS"
 
 args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 0) args <- c((today() - days(2)) %>% format("%Y%m%d"))
+if (length(args) == 0) args <- c((today() - days(1)) %>% format("%Y%m%d"))
 if (length(args) < 3) args <- c(args, "All", "England")
 
 if (!exists("date.data")) date.data <- args[1]
@@ -99,7 +99,7 @@ scenario.name <- paste0(scenario.name, reporting.delay, "day")
 out.dir <- file.path(proj.dir,
                      "model_runs",
                      date.data,
-                     paste0("updated_", scenario.name, "_matrices_", google.data.date,
+                     paste0(scenario.name, "_matrices_", google.data.date,
 							"_", data.desc))	# Value actually used
 if (!hosp.flag) out.dir <- paste0(out.dir, "_no_deaths")
 if (gp.flag) out.dir <- paste0(out.dir, "_with_linelist")
