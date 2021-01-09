1351f281cea93f6198e87dc6bc545de8b873e847
Joshua Blake <joshua.blake@mrc-bsu.cam.ac.uk>
2021-01-09 20:15:53 +0000

    Customise config for this week's runs

--- config.R
+++ config.R
@@ -75,7 +75,7 @@ gp.flag <- 0	# 0 = off, 1 = on
 ## The 'hosp' stream in the code is linked to death data
 hosp.flag <- 1					# 0 = off, 1 = on
 ## Do we want to include prevalence estimates from community surveys in the model?
-prev.flag <- 0
+prev.flag <- 1
 prev.prior <- "Cevik" # "relax" or "long_positive" or "tight
 ## Shall we fix the serological testing specificity and sensitivty?
 fix.sero.test.spec.sens <- FALSE #prev.flag == 1
@@ -147,7 +147,7 @@ date.prev <- ymd("2021-01-04")
 ## Convert that to an analysis day number
 prev.end.day <- date.prev - start.date - 3
 first.prev.day <- date.prev - 74 - start.date + 1
-days.between.prev <- 14
+days.between.prev <- 7
 ## Default system for getting the days on which the likelihood will be calculated.
 prev.lik.days <- rev(seq(from = as.integer(prev.end.day), to = as.integer(first.prev.day), by = -days.between.prev))
 if(prev.flag) scenario.name <- paste0(scenario.name, "_prev", days.between.prev)
