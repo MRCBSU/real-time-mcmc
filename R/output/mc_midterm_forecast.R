7630a83ab00cf703798925978ddde7cf6d35967a
pjbirrell <paul.birrell@mrc-bsu.cam.ac.uk>
2021-04-24 07:11:38 +0100

    Junk commit

--- R/output/mc_midterm_forecast.R
+++ R/output/mc_midterm_forecast.R
@@ -20,7 +20,7 @@ nweeks.ahead <- 8
 
 counterfactual <- FALSE
 
-projections.basedir <- file.path(out.dir, "projections_MTP")
+projections.basedir <- file.path(out.dir, "projections_counter")
 ## ## Enter dates at which it is anticipated that the contact model will change
 ## mm.breaks <- ymd("20201109") + (1:nforecast.weeks * days(7))
 ## ## Forecast projection
@@ -57,6 +57,13 @@ overwrite.matrices <- FALSE
 
 ## ## mod_pars.Rmd specifications that will change - should only be breakpoints and design matrices
 if(prev.flag & all(prior.r1 == 1)) value.r1 <- 7.18
+if(grepl("projections_counter", projections.basedir, fixed = TRUE))
+{
+    value.vac.alpha1 <- rep(0, length(value.vac.alpha1))
+    value.vac.alphan <- rep(0, length(value.vac.alpha2))
+    value.vac.pi1 <- rep(0, length(value.vac.pi1))
+    value.vac.pi2 <- rep(0, length(value.vac.pi2))
+}
 bank.holiday.days.new <- NULL
 ## ## ---------------------------------------------------------------------------------------------
 
@@ -263,7 +270,7 @@ if(prev.flag){
     save.list <- c(save.list, "prevalence")
     dimnames(prevalence) <- dim.list
 }
-save(list = save.list, file = "projections_midterm.RData")
+save(list = save.list, file = "projections_counter.RData")
 
 ## ## ## Housekeeping
 lapply(hosp.data, file.remove)
