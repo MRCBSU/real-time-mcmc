72ae832a4829b602ca983cbdd51a38edca5765a8
pjbirrell <paul.birrell@mrc-bsu.cam.ac.uk>
2021-09-07 12:46:34 +0100

    Correct growth rate code in report-updated.md

--- R/output/spim_format.R
+++ R/output/spim_format.R
@@ -39,10 +39,10 @@ proj.dir <- dirname(dirname(dirname(out.dir)))
 ## projections.file <- "projections_midterm.RData"
 ## scen.text <- "MTP"
 ## save.text <- "MTP"
-projections.file <- "projections_R1.1.RData"
-scen.text <- "MTP R1.1"
-save.text <- "MTP_R_1.1"
-mtp.filter.date <- lubridate::ymd("20210821") ## ymd(date.data)
+projections.file <- "projections_R2.RData"
+scen.text <- "MTP R2.0"
+save.text <- "MTP_R_2.0"
+mtp.filter.date <- lubridate::ymd("20210828") ## ymd(date.data)
 dir.string <- file.path(proj.dir, paste0("spi-forecasts/date_", date.data))
 if(!file.exists(dir.string)) system(paste("mkdir", dir.string))
 nweeks.midterm <- 11
