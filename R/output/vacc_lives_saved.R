24a439ebf7d24ed13f2fe12a3b6386511625492d
pjbirrell <paul.birrell@mrc-bsu.cam.ac.uk>
2021-09-11 11:44:48 +0100

    Junk commit 20210910

--- R/output/vacc_lives_saved.R
+++ R/output/vacc_lives_saved.R
@@ -1,9 +1,11 @@
 standalone <- T
 
 fns <- "deaths_comparison_prev.RData"
+projections <- FALSE
 
 pth <- getwd()
 rmarkdown::render(file.path(Rfile.dir, "vacc_lives_saved.Rmd"),
+                  output_file = paste0("vacc_lives_saved", ifelse(projections, "_proj", ""), ".html"),
                   output_dir = pth,
                   clean = T)
 
