56810577c40aa660cf2081c2bde12b3f2bb4de17
pjbirrell <paul.birrell@mrc-bsu.cam.ac.uk>
2022-04-08 23:43:58 +0100

    junk update

--- R/output/chain_comparison.R
+++ R/output/chain_comparison.R
@@ -22,8 +22,8 @@ thisFile <- function() {
 }
 Rfile.loc <- dirname(thisFile())
 source(file.path(Rfile.loc, "drw.R"))
-colcode.fl <- "Prev690SeroNHSBT_All_NHS28cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_matrices_20220325_timeuse_household_deaths"
-oldcode.fl <- "Prev690SeroNHSBT_All_NHS28cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_matrices_20220325_timeuse_household_deaths_chain2"
+colcode.fl <- "Prev697SeroNHSBT_All_NHScutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220401_timeuse_household_admissions_no_deaths"
+oldcode.fl <- "Prev697SeroNHSBT_All_NHScutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220401_timeuse_household_admissions_no_deaths_chain2"
 
 ## load("mcmc.RData")
 load(file.path(colcode.fl, "tmp.RData"))
