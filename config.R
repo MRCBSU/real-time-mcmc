4db40600d4e9bb2a7a3ae8f795662853c9841362
Paul.Birrell <paul.birrell@phe.gov.uk>
2022-04-15 03:08:36 +0100

    20220414 matrices

--- config.R
+++ config.R
@@ -91,8 +91,8 @@ print(preprocessed_sus_names)
 ## date.adm_sus <- ymd()
 ## date.adm_seb <- ymd()
 
-google.data.date <- format(ymd("20220408"), format = "%Y%m%d")
-matrix.suffix <- "_stable_household"
+google.data.date <- format(ymd("20220414"), format = "%Y%m%d")
+matrix.suffix <- "_timeuse_household"
 
 ## Number of days to run the simulation for.
 ## Including lead-in time, analysis of data and short-term projection
@@ -159,7 +159,7 @@ deaths.flag <- hosp.flag <- 0			# 0 = admissions (by default - can be modified b
 ## Do we want to include prevalence estimates from community surveys in the model?
 prev.flag <- 1
 prev.prior <- "Cevik" # "relax" or "long_positive" or "tight
-num.prev.days <- 704
+num.prev.days <- 709
 ## Shall we fix the serological testing specificity and sensitivty?
 exclude.eldest.prev <- FALSE
 
@@ -214,13 +214,9 @@ if (data.desc == "all") {
 use.previous.run.for.start <- TRUE
 if(use.previous.run.for.start){
     if(region.type == "NHS"){
-        if(str.cutoff == "60")
-            previous.run.to.use <- file.path(proj.dir, "model_runs", "20220401", paste0("Prev697SeroNHSBT_All_NHS", "28", "cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220401", matrix.suffix, "_", ifelse(hosp.flag, "deaths", "admissions_no_deaths"), c("_chain2", ""))
-                                             )
-        else previous.run.to.use <- file.path(proj.dir, "model_runs", "20220401", paste0("Prev697SeroNHSBT_All_NHS", str.cutoff, "cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220401", matrix.suffix, "_", ifelse(hosp.flag, "deaths", "admissions_no_deaths"), c("_chain2", ""))
-                                              )
+        previous.run.to.use <- file.path(proj.dir, "model_runs", "20220408", paste0("Prev704SeroNHSBT_All_NHS", str.cutoff, "cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220408", matrix.suffix, "_", ifelse(hosp.flag, "deaths", "admissions_no_deaths"), c("_chain2", ""))                                      )
     } else if(region.type == "ONS")
-        previous.run.to.use <- file.path(proj.dir, "model_runs", "20220401", paste0("Prev697SeroNHSBT_All_ONS", str.cutoff, "cutoff_IFR7bp_11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220401", matrix.suffix, "_", ifelse(hosp.flag, "deaths", "admissions_no_deaths"), c("_chain2", ""))
+        previous.run.to.use <- file.path(proj.dir, "model_runs", "20220408", paste0("Prev704SeroNHSBT_All_ONS", str.cutoff, "cutoff_IFR7bp_", ifelse(admissions_only.flag & !hosp.flag, "admissions_only_", ""), "11wk2_prev14-0PHE_3dose_new_mprior_matrices_20220408", matrix.suffix, "_", ifelse(hosp.flag, "deaths", "admissions_no_deaths"), c("_chain2", ""))
                                          )
 }
 iteration.number.to.start.from <- 1 ## 6400
@@ -260,7 +256,7 @@ if(gp.flag){
 prev.cutoff.days <- 2
 prev.days.to.lose <- 0
 ## Convert that to an analysis day number
-date.prev <- lubridate::ymd("20220406")
+date.prev <- lubridate::ymd("20220411")
 prev.end.day <- date.prev - start.date - (prev.cutoff.days - 1) ## Last date in the dataset
 last.prev.day <- prev.end.day - prev.days.to.lose ## Which is the last date that we will actually use in the likelihood?
 first.prev.day <- prev.end.day - num.prev.days + 1
@@ -280,7 +276,7 @@ scenario.name <- paste0(scenario.name, efficacies)
 
 ########### VACCINATION OPTIONS ###########
 vacc.flag <- 1 ## Do we have any vaccination data
-str.date.vacc <- "20220407" ## Optional: if not specified will take the most recent data file.
+str.date.vacc <- "20220413" ## Optional: if not specified will take the most recent data file.
 vacc.lag <- 21
 vac.overwrite <- FALSE
 if(vacc.flag){
